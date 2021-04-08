063d4ef6ab8345c5872148635ef634145f395680
scorpion <saeedalbooyeh@gmail.com>
2021-04-08 21:04:43 +0430

    adding some extra styles to curent pages

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main_page.html')),
+    path('', TemplateView.as_view(template_name='first_page.html')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('catalog/', include('catalog.urls')),
