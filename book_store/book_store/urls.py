fcec35c969084637a0e05dbbc420be56b74ab5a1
scorpion <saeedalbooyeh@gmail.com>
2021-04-09 20:01:12 +0430

    adding allauth templates to replace the defaulets because they were english and some styles adding profile template and url for it in setting and also adding mail server for developing

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='first_page.html')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('user/', include('users.urls')),
     path('catalog/', include('catalog.urls')),
 ]
