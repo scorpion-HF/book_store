3bb98bc01ac025dfddb8439964929b313cd3c5d9
scorpion <saeedalbooyeh@gmail.com>
2021-04-26 17:49:53 +0430

    adding extra styles adding template tag for numbers in persian

---
+++ book_store/catalog/templatetags/IR.py
@@ -0,0 +1,34 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='persian_int')
+def persian_int(english_int):
+    devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
+    number = str(english_int)
+    return ''.join(devanagari_nums[int(digit)] for digit in number)
+
+
+@register.filter(name='persian_date')
+def persian_date(english_date):
+    devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
+    date = ''
+    for i in english_date:
+        if i == '/':
+            date += '/'
+        else:
+            date += devanagari_nums[int(i)]
+    return date
+
+
+@register.filter(name='price')
+def price(input_int):
+    devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
+    persian_price = ''
+    for i in f'{int(input_int):,d}':
+        if i == ',':
+            persian_price += ','
+        else:
+            persian_price += devanagari_nums[int(i)]
+    return persian_price
