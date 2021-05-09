ddfe070fe3a83dfae8711d1df59832be577ffbd6
scorpion <saeedalbooyeh@gmail.com>
2021-05-09 17:45:08 +0430

    pagination added to template in books_list

--- book_store/catalog/views.py
+++ book_store/catalog/views.py
@@ -4,7 +4,7 @@ from .models import Book
 
 class BooksListView(ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 2
     template_name = 'catalog/books_list.html'
     context_object_name = 'books'
     ordering = ['-date_of_publish']
