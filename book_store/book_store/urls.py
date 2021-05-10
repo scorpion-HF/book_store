2df24c28c0293813ffdf7e675bb57446323ae4d4
scorpion <saeedalbooyeh@gmail.com>
2021-05-10 19:51:33 +0430

    admin login disabled and there is one login with capcha

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='first_page.html'), name='landing'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    re_path(r'^admin/login/?$', RedirectView.as_view(pattern_name='account_login')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('captcha/', include('captcha.urls')),
