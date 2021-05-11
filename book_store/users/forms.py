b912f8de4d32cdcac6cd506b51c4149238c77ad7
scorpion <saeedalbooyeh@gmail.com>
2021-05-11 15:25:16 +0430

    capthca form alert problem solved

--- book_store/users/forms.py
+++ book_store/users/forms.py
@@ -1,4 +1,8 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 from django.forms import ModelForm, TextInput, Textarea
+from django.urls import reverse_lazy
+
 from .models import User
 from allauth.account.forms import SignupForm, LoginForm
 from captcha.fields import CaptchaField
@@ -24,3 +28,16 @@ class AllauthSignupForm(SignupForm):
 
 class AllauthLoginForm(LoginForm):
     captcha = CaptchaField(label='حاصل عبارت را وارد نمایید:')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'  # this line sets your form's method to post
+        self.helper.form_action = reverse_lazy('account_login')  # this line sets the form action
+        self.helper.layout = Layout(
+            'login',
+            'password',
+            'remember',
+            'captcha',
+            Submit('submit', u'Submit', css_class='btn btn-success'),
+        )
