40bc2f7f5fa28c553f9b12ef86248c3179387265
saeed <saeedalbooyeh@gmail.com>
2021-06-12 10:42:07 +0430

    locale settings uncommented

--- book_store/book_store/settings.py
+++ book_store/book_store/settings.py
@@ -144,8 +144,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-#LANGUAGE_CODE = 'fa-ir'
-#locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
+LANGUAGE_CODE = 'fa-ir'
+locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
 
 TIME_ZONE = 'Asia/Tehran'
 
